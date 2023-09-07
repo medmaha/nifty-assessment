e3f2f1816b91ab3ae2a80ffecbb3f2350040b04a
= <=>
2023-09-07 00:24:14 +0000

    automated admin user creation

--- app/apps.py
+++ app/apps.py
@@ -1,11 +1,23 @@
 from django.apps import AppConfig
 
+from django.contrib.auth import get_user_model
+
 
 class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
 
     def ready(self) -> None:
-        from . import generators
+        # from . import generators
+        user_model = get_user_model()
+        admin = user_model.objects.filter(username="admin", is_superuser=True).exists()
+        if not admin:
+            admin = user_model.objects.create(username="admin", email="admin@admin.com")
+            admin.set_password("admin")
+            admin.is_active = True
+            admin.is_staff = True
+            admin.is_superuser = True
+            admin.save()
+            print("[Sudo admin-user] --> Created just now")
 
         return super().ready()
